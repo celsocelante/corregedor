579e680f15fa52726414902d4bca155f354ff8c4
Celso Junior <celsojr100@gmail.com>
2014-12-25 22:01:36 -0200

    Pequenas alterações no script

--- corregedor.py
+++ corregedor.py
@@ -25,32 +25,32 @@ dicionario = {
 				"jente":"gente",
 				"presizo":"preciso",
 				"concegui":"consegui",
-				"revoutado":"revoltado"
+				"revoutado":"revoltado",
+				"aki":"aqui"
 			}
-# Mensagens que mudam aleatoriamente para não deixar as respostas tão robóticas
-mensagens = [u"Você sabia que o correto é ", u"Que tal ",u"Já pensou em escrever ",u"Não acha melhor ",u"Olha, já pensou em escrever ", "Dica: sabia que o correto é ", "Você cometeu um erro. Sabia que o certo é "]
 
-try:
-	for chave, valor in dicionario.iteritems():
-		query = chave + " -RT" # Constrói a query da pesquisa, ignorando RTs
-		public_tweets = api.search(query,"pt")
-		# Trabalha com os 3 mais recentes Tweets encontrados
-		for tweet in public_tweets[0:3]:
-			id_tweet = tweet.id
-			# Se o Tweet já foi respondido antes, pula para o próximo
-			if str(id_tweet) in tweets_respondidos:
-				print "Tweet já respondido, pulando para o próximo"
-				continue
-			usuario = tweet.user.screen_name
-			time.sleep(random.uniform(0.26, 4.79)) # Para o script em tempos aleatórios
-			reply = "@" + usuario + " " + random.choice(mensagens) + "\"" + valor + "\"" + "?"
-			# Escreve no banco de Tweets
-			f.write(str(id_tweet)+"\n")
-			# Envia reply
-			api.update_status(reply,id_tweet)
-			# Faz RT
-			api.retweet(id_tweet)
-			print reply
-except TweepError:
-	print "Erro com a API do Twitter :("
+# Mensagens que mudam aleatoriamente para não deixar as respostas tão robóticas
+mensagens = [u"Você sabia que o correto é ", u"Que tal ", u"Já pensou em escrever ", u"Não acha melhor ", u"Olha, já pensou em escrever ", u"Dica: sabia que o correto é ", u"Você cometeu um erro. Sabia que o certo é "]
+
+for chave, valor in dicionario.iteritems():
+	query = chave + " -RT" # Constrói a query da pesquisa, ignorando RTs
+	public_tweets = api.search(query,"pt")
+	# Trabalha com os 3 mais recentes Tweets encontrados
+	for tweet in public_tweets[0:2]:
+		id_tweet = tweet.id
+		# Se o Tweet já foi respondido antes, pula para o próximo
+		if str(id_tweet) in tweets_respondidos:
+			print "[*] Tweet já respondido, pulando para o próximo"
+			continue
+		usuario = tweet.user.screen_name
+		time.sleep(random.uniform(0.26, 4.79)) # Para o script em tempos aleatórios
+		reply = "@" + usuario + " " + random.choice(mensagens) + "\"" + valor + "\"" + "?"
+		# Escreve no banco de Tweets
+		f.write(str(id_tweet)+"\n")
+		# Envia reply
+		api.update_status(reply,id_tweet)
+		# Faz RT
+		api.retweet(id_tweet)
+		print reply
+
 f.close()
